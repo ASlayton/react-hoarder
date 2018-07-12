6fa9baab577ae93c7da1ac033e98c279138bee61
Amanda Slayton <alslayton86@gmail.com>
2018-07-11 21:36:38 -0500

    wip

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,47 @@ import {Route, BrowserRouter, Redirect, Switch}  from 'react-router-dom';
 import firebase from 'firebase';
 import Navbar from '../components/Navbar/Navbar';
 import fbConnection from '../firebaseRequests/connection';
+import Home from '../components/Home/Home';
+import MyStuff from '../components/MyStuff/MyStuff';
+import Register from '../components/Register/Register';
+import Login from '../components/Login/Login';
 import './App.css';
 fbConnection();
+
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/MyStuff', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/allTheThings', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends React.Component {
   state={
     authed: false,
@@ -37,6 +76,26 @@ class App extends React.Component {
               authed={this.state.authed}
               runAway={this.runAway}
             />
+          </div>
+          <div className="main-container">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <PrivateRoute
+                path="/myStuff"
+                authed={this.state.authed}
+                component={MyStuff}
+              />
+              <PublicRoute
+                path="/register"
+                authed={this.state.authed}
+                component={Register}
+              />
+              <PublicRoute
+                path="/login"
+                authed={this.state.authed}
+                component={Login}
+              />
+            </Switch>
           </div>
         </BrowserRouter>
       </div>
---
+++ src/components/MyStuff/MyStuff.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyStuff.css';
+
+class MyStuff extends React.Component {
+  render () {
+    return (
+      <div>My Stuff</div>
+    );
+  }
+}
+
+export default MyStuff;
